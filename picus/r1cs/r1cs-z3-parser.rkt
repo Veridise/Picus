98e7c737e9eb8b2ffc83770797d65fa02b251a27
sorawee <sorawee.pwase@gmail.com>
2023-08-28 16:39:35 -0700

    fix: make constraint generation more efficient (#13)
    * chore: remove dead code
    
    * fix: make constraint generation more efficient
    
    Prior to this commit, we use list as a data structure to keep inputs,
    and then access them by indices, which is inefficient.
    This commit fixes the problem by converting the list to vector
    for vector indexing. On the performance benchmark, it reduces
    the runnning time for constraint generation from 8 minutes to 1 second.

--- picus/r1cs/r1cs-z3-parser.rkt
+++ picus/r1cs/r1cs-z3-parser.rkt
@@ -6,11 +6,8 @@
     (prefix-in config: "../config.rkt")
     (prefix-in r1cs: "./r1cs-grammar.rkt")
 )
-(provide (rename-out
-    [parse-r1cs parse-r1cs]
-    [expand-r1cs expand-r1cs]
-    [parse-r1cs-old parse-r1cs-old]
-))
+(provide parse-r1cs
+         expand-r1cs)
 
 ; turns r1cs binary form into standard form
 ; arguments:
@@ -32,17 +29,19 @@
     ; first create a list of all symbolic variables according to nwires
     (define nwires (r1cs:get-nwires arg-r1cs))
     ; strictly align with wid
-    (define xlist (if (null? arg-xlist)
-        ; create fresh new
-        (for/list ([i nwires]) (format "x~a" i))
-        ; use existing one
-        arg-xlist
-    ))
+    (define xvec
+      (match arg-xlist
+        ['()
+         ; create fresh new
+         (for/vector ([i nwires]) (format "x~a" i))]
+        [_
+         ; use existing one
+         (list->vector arg-xlist)]))
 
     ; add declarations for variables
     (set! raw-decls (append raw-decls
         (list (r1cs:rcmt "======== declaration constraints ========"))
-        (for/list ([x xlist])
+        (for/list ([x xvec])
             (if (and (not (null? arg-xlist)) (string-prefix? x "x"))
                 ; provided list with already defined x, skip
                 (r1cs:rcmt (format "~a: already defined" x))
@@ -55,7 +54,7 @@
     ; add range constraints for declared variables
     (set! raw-decls (append raw-decls
         (list (r1cs:rcmt "======== range constraints ========"))
-        (for/list ([x xlist])
+        (for/list ([x xvec])
             (if (and (not (null? arg-xlist)) (string-prefix? x "x"))
                 ; provided list with already defined x, skip
                 (r1cs:rcmt (format "~a: already defined" x))
@@ -99,19 +98,19 @@
         ; note that terms could be empty, in which case 0 is used
         (define terms-a (cons (r1cs:rint 0) (for/list ([w0 wids-a] [f0 factors-a])
             ; (format "(* ~a ~a)" f0 x0)
-            (let ([x0 (list-ref xlist w0)])
+            (let ([x0 (vector-ref xvec w0)])
                 (r1cs:rmul (list (r1cs:rint f0) (r1cs:rvar x0)))
             )
         )))
         (define terms-b (cons (r1cs:rint 0) (for/list ([w0 wids-b] [f0 factors-b])
             ; (format "(* ~a ~a)" f0 x0)
-            (let ([x0 (list-ref xlist w0)])
+            (let ([x0 (vector-ref xvec w0)])
                 (r1cs:rmul (list (r1cs:rint f0) (r1cs:rvar x0)))
             )
         )))
         (define terms-c (cons (r1cs:rint 0) (for/list ([w0 wids-c] [f0 factors-c])
             ; (format "(* ~a ~a)" f0 x0)
-            (let ([x0 (list-ref xlist w0)])
+            (let ([x0 (vector-ref xvec w0)])
                 (r1cs:rmul (list (r1cs:rint f0) (r1cs:rvar x0)))
             )
         )))
@@ -133,12 +132,12 @@
         (list (r1cs:rcmt "======== main constraints ========"))
         sconstraints
         (list (r1cs:rassert (r1cs:req
-            (r1cs:rint 1) (r1cs:rvar (format "~a" (list-ref xlist 0)))
+            (r1cs:rint 1) (r1cs:rvar (format "~a" (vector-ref xvec 0)))
         )))
     ))
 
     (values
-        xlist
+        (vector->list xvec)
         (r1cs:rcmds raw-opts)
         (r1cs:rcmds raw-decls)
         (r1cs:rcmds raw-cnsts)
@@ -212,183 +211,4 @@
     ))
     ; return
     (r1cs:rcmds ret-cnsts)
-)
-
-; to be deleted
-(define (parse-r1cs-old arg-r1cs arg-xlist)
-
-    ; a list of options
-    (define raw-options (list
-        (r1cs:rlogic "QF_NIA")
-    ))
-
-    (define raw-cmds (list)) ; a list of commands
-    (define raw-declarations (list)) ; a list of variable declarations
-
-    ; first create a list of all symbolic variables according to nwires
-    (define nwires (r1cs:get-nwires arg-r1cs))
-    ; strictly align with wid
-    (define xlist (if (null? arg-xlist)
-        ; create fresh new
-        (for/list ([i nwires]) (format "x~a" i))
-        ; use existing one
-        arg-xlist
-    ))
-
-    ; add declarations for variables
-    (set! raw-declarations (append raw-declarations
-        (list (r1cs:rcmt "======== declaration constraints ========"))
-        (for/list ([x xlist])
-            (if (and (not (null? arg-xlist)) (string-prefix? x "x"))
-                ; provided list with already defined x, skip
-                (r1cs:rcmt (format "~a: already defined" x))
-                ; otherwise you need to define this variable
-                (r1cs:rdef (r1cs:rvar (format "~a" x)) (r1cs:rtype "Int"))
-            )
-        )
-    ))
-    
-    ; add range constraints for declared variables
-    (set! raw-declarations (append raw-declarations
-        (list (r1cs:rcmt "======== range constraints ========"))
-        (for/list ([x xlist])
-            (if (and (not (null? arg-xlist)) (string-prefix? x "x"))
-                ; provided list with already defined x, skip
-                (r1cs:rcmt (format "~a: already defined" x))
-                ; otherwise you need to define this variable
-                (r1cs:rassert (r1cs:rand (list
-                    (r1cs:rleq (r1cs:rint 0) (r1cs:rvar (format "~a" x)))
-                    (r1cs:rlt (r1cs:rvar (format "~a" x)) (r1cs:rint config:p))
-                )))
-            )
-        )
-    ))
-
-    ; then start creating constraints
-    (define m (r1cs:get-mconstraints arg-r1cs))
-    (define rconstraints (r1cs:get-constraints arg-r1cs)) ; r1cs constraints
-
-    ; symbolic constraints
-    (define sconstraints (for/list ([cnst rconstraints])
-
-        ; get block
-        (define curr-block-a (r1cs:constraint-a cnst))
-        (define curr-block-b (r1cs:constraint-b cnst))
-        (define curr-block-c (r1cs:constraint-c cnst))
-
-        ; process block a
-        (define nnz-a (r1cs:constraint-block-nnz curr-block-a))
-        (define wids-a (r1cs:constraint-block-wids curr-block-a))
-        (define factors-a (r1cs:constraint-block-factors curr-block-a))
-
-        ; process block b
-        (define nnz-b (r1cs:constraint-block-nnz curr-block-b))
-        (define wids-b (r1cs:constraint-block-wids curr-block-b))
-        (define factors-b (r1cs:constraint-block-factors curr-block-b))
-
-        ; process block c
-        (define nnz-c (r1cs:constraint-block-nnz curr-block-c))
-        (define wids-c (r1cs:constraint-block-wids curr-block-c))
-        (define factors-c (r1cs:constraint-block-factors curr-block-c))
-
-        ; ======== parse into original form: A*B = C ========
-        ; assemble symbolic terms
-        ; note that terms could be empty, in which case 0 is used
-        ; (define terms-a (cons (r1cs:rint 0) (for/list ([w0 wids-a] [f0 factors-a])
-        ;     ; (format "(* ~a ~a)" f0 x0)
-        ;     (let ([x0 (list-ref xlist w0)])
-        ;         (r1cs:rmul (list (r1cs:rint f0) (r1cs:rvar x0)))
-        ;     )
-        ; )))
-        ; (define terms-b (cons (r1cs:rint 0) (for/list ([w0 wids-b] [f0 factors-b])
-        ;     ; (format "(* ~a ~a)" f0 x0)
-        ;     (let ([x0 (list-ref xlist w0)])
-        ;         (r1cs:rmul (list (r1cs:rint f0) (r1cs:rvar x0)))
-        ;     )
-        ; )))
-        ; (define terms-c (cons (r1cs:rint 0) (for/list ([w0 wids-c] [f0 factors-c])
-        ;     ; (format "(* ~a ~a)" f0 x0)
-        ;     (let ([x0 (list-ref xlist w0)])
-        ;         (r1cs:rmul (list (r1cs:rint f0) (r1cs:rvar x0)))
-        ;     )
-        ; )))
-        ; (define sum-a (r1cs:radd terms-a))
-        ; (define sum-b (r1cs:radd terms-b))
-        ; (define sum-c (r1cs:radd terms-c))
-        ; ; original form: A*B = C
-        ; (define ret-cnst (r1cs:rassert (r1cs:req
-        ;     (r1cs:rmod (r1cs:rmul (list sum-a sum-b)) (r1cs:rint config:p))
-        ;     (r1cs:rmod sum-c (r1cs:rint config:p))
-        ; )))
-
-        ; ======== parse into expanded form: A*B = C but expand A*B into ++terms ========
-        ; assemble symbolic terms
-        ; note that terms could be empty, in which case 0 is used
-        (define terms-a (for/list ([w0 wids-a] [f0 factors-a])
-            ; (format "(* ~a ~a)" f0 x0)
-            (let ([x0 (list-ref xlist w0)])
-                (cons f0 x0)
-            )
-        ))
-        (define terms-b (for/list ([w0 wids-b] [f0 factors-b])
-            ; (format "(* ~a ~a)" f0 x0)
-            (let ([x0 (list-ref xlist w0)])
-                (cons f0 x0)
-            )
-        ))
-        (define terms-c (for/list ([w0 wids-c] [f0 factors-c])
-            ; (format "(* ~a ~a)" f0 x0)
-            (let ([x0 (list-ref xlist w0)])
-                (cons f0 x0)
-            )
-        ))
-        ; A*B = C but expand A*B into ++terms
-        (define ret-cnst (r1cs:rassert (r1cs:req
-            ; A*B part
-            (if (or (empty? terms-a) (empty? terms-b))
-                ; A*B is empty
-                (r1cs:rint 0)
-                ; A*B is not empty
-                (r1cs:rmod
-                    (r1cs:radd (for*/list ([va terms-a][vb terms-b])
-                        (r1cs:rmul (list
-                            (r1cs:rint (remainder (* (car va) (car vb)) config:p))
-                            (r1cs:rvar (cdr va))
-                            (r1cs:rvar (cdr vb))
-                        ))
-                    ))
-                    (r1cs:rint config:p)
-                )
-            )
-            ; C part
-            (if (empty? terms-c)
-                ; C is empty
-                (r1cs:rint 0)
-                ; C is not empty
-                (r1cs:rmod
-                    (r1cs:radd (for/list ([v terms-c]) (r1cs:rmul (list (r1cs:rint (car v)) (r1cs:rvar (cdr v))))))
-                    (r1cs:rint config:p)
-                )
-            )
-        )))
-
-        ; return this assembled constraint
-        ret-cnst
-    ))
-
-    ; update smt with comments and fixed constraints
-    (set! raw-cmds (append
-        raw-cmds
-        sconstraints
-        (list (r1cs:rassert (r1cs:req
-            (r1cs:rint 1) (r1cs:rvar (format "~a" (list-ref xlist 0)))
-        )))
-    ))
-
-    (values
-        xlist
-        (r1cs:rcmds raw-options)
-        (r1cs:rcmds raw-declarations)
-        (r1cs:rcmds raw-cmds)
-    )
 )
