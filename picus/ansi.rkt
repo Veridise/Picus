922fbf719580a852a305e912632c6007b6e2fd04
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2023-10-22 17:50:18 +0700

    logging: strip ANSI code

--- picus/ansi.rkt
+++ picus/ansi.rkt
@@ -1,9 +1,21 @@
 #lang racket
 
-(provide highlight)
+(provide highlight
+         strip-ansi)
 
 (define (ansi-code code)
   (format "~a~a" (integer->char #x1b) code))
 
 (define (highlight s)
   (~a (ansi-code "[33m") s (ansi-code "[0m")))
+
+(define (strip-ansi s)
+  (regexp-replace*
+   (string-append (regexp-quote (string (integer->char #x1b)))
+                  "\\[[0-9;]*m")
+   s
+   ""))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (strip-ansi (highlight "abc")) "abc"))
