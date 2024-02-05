154ee76357dfd5710a15df2f23f72d23a3858423
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2024-02-05 11:27:21 -0800

    ci: make adjustments to improve ci

--- tests/circomlib-test.rkt
+++ tests/circomlib-test.rkt
@@ -3,9 +3,12 @@
 (module+ test
   (require "testlib.rkt")
 
+  ;; Failed with cvc5-bitsum
+  (check-result (make-run-config "circomlib-cff5ab6/BabyDbl@babyjub.circom") 'safe)
+  (check-result (make-run-config "circomlib-cff5ab6/MontgomeryDouble@montgomery.circom") 'unsafe)
+
   (check-result (make-run-config "circomlib-cff5ab6/AND@gates.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/BabyAdd@babyjub.circom") 'unknown)
-  (check-result (make-run-config "circomlib-cff5ab6/BabyDbl@babyjub.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/BabyPbk@babyjub.circom") 'timeout)
   (check-result (make-run-config "circomlib-cff5ab6/BinSub@binsub.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/BinSum@binsum.circom") 'safe)
@@ -35,7 +38,6 @@
   (check-result (make-run-config "circomlib-cff5ab6/MiMCSponge@mimcsponge.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/Montgomery2Edwards@montgomery.circom") 'unsafe)
   (check-result (make-run-config "circomlib-cff5ab6/MontgomeryAdd@montgomery.circom") 'unsafe)
-  (check-result (make-run-config "circomlib-cff5ab6/MontgomeryDouble@montgomery.circom") 'unsafe)
   (check-result (make-run-config "circomlib-cff5ab6/MultiAND@gates.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/MultiMiMC7@mimc.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/MultiMux1@mux1.circom") 'safe)
@@ -52,7 +54,7 @@
   (check-result (make-run-config "circomlib-cff5ab6/NOR@gates.circom") 'safe)
   (check-result (make-run-config "circomlib-cff5ab6/NOT@gates.circom") 'safe)
 
-  ;; was safe, but the basis lemma fix made it timeouted
+  ;; was safe, but the basis lemma fix made it timed out
   (check-result (make-run-config "circomlib-cff5ab6/Num2Bits_strict@bitify.circom") 'timeout)
 
   (check-result (make-run-config "circomlib-cff5ab6/Num2Bits@bitify.circom") 'safe)
@@ -63,7 +65,7 @@
   ;; NOTE: unknown (with longer time limit)
   (check-result (make-run-config "circomlib-cff5ab6/Pedersen@pedersen.circom") 'timeout)
 
-  ;; was safe, but the basis lemma fix made it timeouted
+  ;; was safe, but the basis lemma fix made it timed out
   (check-result (make-run-config "circomlib-cff5ab6/Point2Bits_Strict@pointbits.circom") 'timeout)
 
   (check-result (make-run-config "circomlib-cff5ab6/Poseidon@poseidon.circom") 'safe)
